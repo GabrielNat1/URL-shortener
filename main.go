dfe05fe4ec95f1f3c67d7ab414e8321c7cab3afd
GabrielNat1 <dcbiel846@gmail.com>
2025-04-24 15:12:42 -0300

    Fix environment variable naming and improve initialization in main.go

--- main.go
+++ main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 var (
-	urlStore = make(map[string]string)
-	secretKey = 
+	urlStore  = make(map[string]string)
+	secretKey string
 )
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	secretKey = os.Getenv("SECRET_KEY")
+}
+
 func encrypt(url string) string {
+	// -----------------------------------
+
 	//aes.NewCipher
 }
 
-
 func shorterUrl(w http.ResponseWriter, r *http.Request) {
 	initial_url := r.URL.Query().Get("url")
-	if initial_url== "" {
+	if initial_url == "" {
 		http.Error(w, "URL is required", http.StatusBadRequest)
 		return
 	}
