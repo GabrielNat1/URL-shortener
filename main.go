ab4f32494a191941b732d985c682af4e8cbc32a7
GabrielNat1 <dcbiel846@gmail.com>
2025-04-27 21:37:28 -0300

    fix: simplify contains function to return true when item is found

--- main.go
+++ main.go
@@ -330,7 +330,7 @@ func cleanupExpiredUrls() {
 func contains(slice []string, item string) bool {
 	for _, s := range slice {
 		if s == item {
-			return s == item
+			return true
 		}
 	}
 	return false
